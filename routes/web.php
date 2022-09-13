bde9387c5d09e8dc22f19d37643206a022bbe4ce
FUJIKAWA-IKU <fujikawaiku.potepan@gmail.com>
2022-09-13 14:24:28 +0900

    ドットインストール基本機能編

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', ['App\Http\Controllers\PostController', 'index']);
+
+// Route::get('/', [App\Http\Controllers\PostController::class, 'index']);
+
+Route::get('/', [PostController::class, 'index'])
+    ->name('posts.index');
+
+Route::get('/posts/{id}', [PostController::class, 'show'])
+    ->name('posts.show');;
